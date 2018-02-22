a1aabfe1a99692a0e0727dee0c0b6899023d19c1
Ramie Blatt <rblatt@warmlyyours.com>
2018-02-22 16:46:03 -0700

    using scope for url generation in api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  serialization_scope :view_context
 end
--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,9 +1,8 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :first_name, :last_name, :given_name, :birthday, :birth_country, :birth_state, :birth_city, :death, :death_country, :death_state, :death_city, :weight, :height, :bats, :throws, :debut, :final_game, :lahman_player_id, :url
-  include Rails.application.routes.url_helpers
 
   def url
-    player_url(object, format: :json, host: ENV['EXTERNAL_HOST'] || 'localhost:3000',
-  protocol: ENV['EXTERNAL_HOST_PROTOCOL'] || 'http')
+    scope.player_url(object, format: :json)
   end
+
 end
