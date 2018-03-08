3f6f84f098b62a560fa6b900ad919cd7eeddf4b9
Ramie Blatt <rblatt@warmlyyours.com>
2018-03-08 12:37:53 -0700

    adding cerulean bootswatch styling

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
       post :remove_all_cards
     end
   end
+  root to: 'players#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
