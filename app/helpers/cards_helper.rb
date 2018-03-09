aac1aded963d4a245b83848311c5aafba1395249
Ramie Blatt <rblatt@warmlyyours.com>
2018-03-08 18:56:30 -0700

    more refinements

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,17 @@
 module CardsHelper
+    def card_links_to_bbref(card)
+    res = ""
+    player = card.player
+    if player.is_mlbp? or (!player.is_manager?)
+      res << link_to("#{player.lahman_bbref_id}", "https://www.baseball-reference.com/#{player.is_mlbp? ? 'players/'+player.lahman_bbref_id.to_s[0] : 'nonmlbpa'}/#{player.lahman_bbref_id}.shtml", target: '_new')
+    end
+    if player.is_manager?
+      if player.is_mlbp?
+        res << raw(" | ")
+      end
+      res << link_to("#{player.lahman_bbref_id} (as mgr)", "https://www.baseball-reference.com/managers/#{player.lahman_bbref_id}.shtml", target: '_new_mgr')
+    end
+    return raw(res)
+  end
+
 end
